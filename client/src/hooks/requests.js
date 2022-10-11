ff9755431fa40c4f5814865ef8b7a2ecd3a19bdc
Giovanni Martin <71309186+giolmartin@users.noreply.github.com>
2022-10-11 02:05:36 +0200

    csv parse and httpGetPlanets done

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,11 +1,15 @@
+const API_URL = "http://localhost:8000";
+
+// Load planets and return as JSON.
 async function httpGetPlanets() {
-  // TODO: Once API is ready.
-  // Load planets and return as JSON.
+  const response = await fetch(`${API_URL}/planets`);
+
+  return await response.json();
 }
 
+// Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
   // TODO: Once API is ready.
-  // Load launches, sort by flight number, and return as JSON.
 }
 
 async function httpSubmitLaunch(launch) {
@@ -18,9 +22,4 @@ async function httpAbortLaunch(id) {
   // Delete launch with given ID.
 }
 
-export {
-  httpGetPlanets,
-  httpGetLaunches,
-  httpSubmitLaunch,
-  httpAbortLaunch,
-};
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
