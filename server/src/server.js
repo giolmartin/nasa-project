94e6eab5e9dadd5e9d4a4733a708143ea1b1d04f
Giovanni Martin <71309186+giolmartin@users.noreply.github.com>
2022-10-20 16:21:31 +0200

    test unit updated and passed, set tot E2E tests, need new test database if is going to test often.

--- server/src/server.js
+++ server/src/server.js
@@ -1,30 +1,17 @@
 const http = require("http");
-const mongoose = require("mongoose");
 
 const app = require("./app");
-
+const { mongoConnect } = require("./services/mongo");
 const { loadPlanetsData } = require("./models/planets.model");
 
 //At package.json on "scripts: {"start":" PORT=port_number node server.js"}"
 //if ports needs to be specified
 const PORT = process.env.PORT || 8000;
 
-//Change password before commiting
-const MONGO_URL =
-  "mongodb+srv://nasa-api:Amwvc2Cz4094O6Cr@nasacluster.e6wuq5j.mongodb.net/nasa?retryWrites=true&w=majority";
-
 const server = http.createServer(app);
 
-mongoose.connection.once("open", () => {
-  console.log("MongoDB connection open");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(MONGO_URL);
+  await mongoConnect();
   await loadPlanetsData();
 
   server.listen(PORT, () => {
---
+++ server/src/services/mongo.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const MONGO_URL =
+  "mongodb+srv://nasa-api:Amwvc2Cz4094O6Cr@nasacluster.e6wuq5j.mongodb.net/nasa?retryWrites=true&w=majority";
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection open");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
+async function mongoConnect() {
+  await mongoose.disconnect();
+  await mongoose.connect(MONGO_URL);
+}
+async function mongoDisconnect() {
+  await mongoose.connection.close();
+}
+
+module.exports = {
+  mongoConnect,
+  mongoDisconnect,
+};
