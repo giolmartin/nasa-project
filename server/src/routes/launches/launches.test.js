e4f0d981fe9e03ebf7e36292beb059f72768e210
Giovanni Martin <71309186+giolmartin@users.noreply.github.com>
2023-02-09 12:07:34 -0600

    Trying to fix the fsevents@2.3.2: error at workflow

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -5,7 +5,7 @@ const { loadPlanetsData } = require('../../models/planets.model');
 
 describe('Launches API', () => {
   beforeAll(async () => {
-    jest.setTimeout(30000);
+    jest.setTimeout(10 * 1000);
     await mongoConnect();
     await loadPlanetsData();
   });
