7e8fb19a994db1ddcd2e9f0238c3d94c0c78258d
Giovanni Martin <71309186+giolmartin@users.noreply.github.com>
2023-02-09 14:25:23 -0600

    passwithnotest flag added

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -5,7 +5,7 @@ const { loadPlanetsData } = require('../../models/planets.model');
 
 describe('Launches API', () => {
   beforeAll(async () => {
-    jest.setTimeout(10 * 1000);
+    jest.setTimeout(30 * 1000);
     await mongoConnect();
     await loadPlanetsData();
   });
